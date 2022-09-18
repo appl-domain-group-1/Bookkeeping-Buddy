1b17679697f12e128cbecdcf0c338877d4708b35
phredmund <eexley@gmail.com>
2022-09-18 08:31:13 -0400

    Refactoring

--- appl_domain/email.py
+++ appl_domain/email.py
@@ -10,6 +10,29 @@ SENDER = 'noreply.bookkeepingbuddy@gmail.com'
 SITE_URL = 'http://localhost:5000'  # TODO: Change this when we go live
 
 
+def __send_email(to, subject, message):
+    # Create empty email object
+    email = EmailMessage()
+    # Set email info
+    email['From'] = SENDER
+    email['Subject'] = subject
+    # Set the content of the email
+    email.set_content(message)
+    # Set the email destination
+    email['To'] = to
+
+    # Create SSL context
+    context = ssl.create_default_context()
+    # Send the email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(SENDER, EMAIL_PASSWORD)
+        smtp.sendmail(SENDER, email["To"], email.as_string())
+
+    # Reference:
+    # https://towardsdatascience.com/how-to-easily-automate-emails-with-python-8b476045c151#bc59
+    # User: noreply.bookkeepingbuddy@gmail.com   Pass: xEcN5GxFjCBqq29
+
+
 def email_registration(username, first_name, last_name):
     # Get a handle on the database
     db = get_db()
@@ -18,36 +41,21 @@ def email_registration(username, first_name, last_name):
         "SELECT * FROM users WHERE role = ?", (2,)
     ).fetchall()
 
+    # Email subject
+    subject = 'Account Verification'
+
     # Craft the URL to be sent in the email
     endpoint = url_for('auth.approve_user', username=username)
-
-    # Create empty email object
-    email = EmailMessage()
-    # Set email info
-    email['From'] = SENDER
-    email['Subject'] = 'Account verification'
-    # Create SSL context
-    context = ssl.create_default_context()
-
     # Loop through each admin and send the email
     for admin in admins:
         # Content to be sent in the email
-        msg = MIMEText(
+        message = MIMEText(
             f"Hello {admin['first_name']} {admin['last_name']},<br><br>{first_name} {last_name} has requested for "
             f"their account to be approved.<br><br><a href='{SITE_URL}{endpoint}'>Click here to activate the "
             f"account</a>.<br><br>If you wish to reject this request, simply ignore this email.<br><br>Regards,<br><br>"
             f"Your Bookkeeping Buddy", 'html')
-        # Set the content of the email
-        email.set_content(msg)
-        # Set the email destination
-        email['To'] = admin["email_address"]
-        # Send it
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-            smtp.login(SENDER, EMAIL_PASSWORD)
-            smtp.sendmail(SENDER, email["To"], email.as_string())
 
-    # https://towardsdatascience.com/how-to-easily-automate-emails-with-python-8b476045c151#bc59
-    # User: noreply.bookkeepingbuddy@gmail.com   Pass: xEcN5GxFjCBqq29
+        __send_email(admin["email_address"], subject, message)
 
 
 def send_approval(username):
@@ -58,23 +66,15 @@ def send_approval(username):
         "SELECT * FROM users WHERE username = ?", (username,)
     ).fetchone()
 
-    # Craft endpoint
-    endpoint = url_for('auth.login')
+    # Email subject
+    subject = 'Account approved'
 
-    # Create empty email object
-    email = EmailMessage()
-    # Set email info
-    email['From'] = SENDER
-    email['Subject'] = 'Account approved'
-    # Create SSL context
-    context = ssl.create_default_context()
+    # Craft endpoint to be linked in the email
+    endpoint = url_for('auth.login')
     # Content to be sent in the email
-    msg = MIMEText(
+    message = MIMEText(
         f"Hello {user['first_name']} {user['last_name']},<br><br>Your new account on Bookkeeping Buddy has been "
         f"activated!<br><br><a href='{SITE_URL}{endpoint}'>Click here to log in.</a><br><br>Regards,<br><br>Your "
         f"Bookkeeping Buddy", 'html')
-    email.set_content(msg)
-    email['To'] = user["email_address"]
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(SENDER, EMAIL_PASSWORD)
-        smtp.sendmail(SENDER, email["To"], email.as_string())
+
+    __send_email(user["email_address"], subject, message)
