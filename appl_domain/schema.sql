6fe7e3b5e1d5ff3594e52ea086068c1b93fe45d6
phredmund <eexley@gmail.com>
2022-09-29 18:34:48 -0400

    Adding table for financial accounts

--- appl_domain/schema.sql
+++ appl_domain/schema.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS users;
+/* Enable foreign key support */
+PRAGMA foreign_keys = ON;
 
+/* Table to store user accounts */
 CREATE TABLE users (
   username TEXT PRIMARY KEY UNIQUE,
   email_address TEXT NOT NULL,
@@ -20,4 +22,46 @@ CREATE TABLE users (
   first_pet TEXT NOT NULL,
   city_born TEXT NOT NULL,
   year_graduated_hs INTEGER NOT NULL
+);
+
+/* Contains different financial account categories */
+CREATE TABLE acct_categories (
+    /* name of this category */
+    name TEXT NOT NULL,
+    /* unique identifier for this category */
+    number INTEGER PRIMARY KEY UNIQUE
+);
+
+/* Contains different financial statements which may be collected */
+CREATE TABLE statements (
+    /* unique identifier for this statement */
+    number INTEGER PRIMARY KEY UNIQUE,
+    /* name of this statement */
+    name TEXT NOT NULL
+);
+
+/* Contains financial accounts */
+CREATE TABLE accounts (
+    acct_name TEXT NOT NULL,
+    acct_num INTEGER PRIMARY KEY UNIQUE,
+    acct_desc TEXT,
+    /* category to which this account belongs. Must be in
+       the acct_categories table */
+    acct_category INTEGER NOT NULL REFERENCES acct_categories(number),
+    /* Account subcategory */
+    acct_subcategory INTEGER NOT NULL,
+    /* debit acct == 1, credit acct == 0 */
+    debit INTEGER NOT NULL,
+    /* stored as cents (no decimals) */
+    initial_bal INTEGER NOT NULL,
+    /* stored as cents (no decimals) */
+    balance INTEGER NOT NULL,
+    /* string in the format YYYY-MM-DD */
+    date_created TEXT NOT NULL,
+    /* must have been created by someone in users table */
+    created_by TEXT NOT NULL REFERENCES users(username),
+    /* financial statement which uses this account */
+    statement INTEGER NOT NULL REFERENCES statements(number),
+    /* Comment about this table */
+    comment TEXT
 );
